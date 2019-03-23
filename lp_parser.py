fc81b09f90ed49c03d47921864204cddea0f2cc8
ApostolisTs <apostolis.tselios@gmail.com>
2019-03-23 13:57:09 +0200

    Add extract_constraints and extract_bconstants functions.
    The extract_constraints function returns the type of constraint
    for a linear equiation and extract_bconstants returns the right hand
    side constants of a linear equations.
    Fix bugs in extract_factors and handle ValueError.

--- lp_parser.py
+++ lp_parser.py
@@ -4,13 +4,13 @@ import os
 # REGULAR EXPRESSIONS
 minmax_regex = re.compile(r'\A(min|max)', re.I)
 objective_function_regex = re.compile(
-    r'((\-|\+)?(\d*|\d+\.\d+)x\d+)((\-|\+)(\d*|\d+\.\d+)x\d+)+$', re.I)
+    r'((\-|\+)?(\d*|\d+\.\d+)?x\d+)((\-|\+)(\d*|\d+\.\d+)?x\d+)+$', re.I)
 st_regex = re.compile(r'\A(st|s\.t\.|subjectto)', re.I)
 tech_contraints_regex = re.compile(
-    r'((\-|\+)?(\d*|\d+\.\d+)x\d+)((\-|\+)(\d*|\d+\.\d+)x\d+)+(>=|<=|=)\d+$', re.I)
+    r'((\-|\+)?(\d*|\d+\.\d+)?x\d+)((\-|\+)(\d*|\d+\.\d+)?x\d+)+(>=|<=|=)\-?\d+$', re.I)
 
 
-def load_data():
+def load_linear_problem():
     """ Return data
 
     Loads the linear problem from a file and return its data stripped,
@@ -102,9 +102,7 @@ def extract_factors(linear_eq):
 
     # A list with tuples containing the sign and factor of each x.
     # e.g. [('', '23.1'), ('-', '0'), ('+', '23'), ('-', '10')]
-    x_factors = re.findall(r'(\-|\+)?(\d+|\d+\.\d+)x\d+', linear_eq)
-    print('x:', x_factors)
-
+    x_factors = re.findall(r'(\-|\+)?(\d+|\d+\.\d+)?x\d+', linear_eq)
     factors = []
 
     # Iterate every tuple in x_factors.
@@ -113,22 +111,82 @@ def extract_factors(linear_eq):
         sign, value = factor
         # If there is no sign and no value or there is a '+' sign and no value
         # the factor is 1.
-        if (sign == None and value == None) or (sign == '+' and value == None):
-            factors.append('1')
+        if (sign == '' and value == '') or (sign == '+' and value == ''):
+            factors.append(1)
         # Else if there is a '-' sign and no value the factor is -1.
-        elif sign == '-' and value == None:
-            factors.append('-1')
+        elif sign == '-' and value == '':
+            factors.append(-1)
         # Else if there is no sign or the sign is '-' and the value is '0'
         # The factor is the value.
-        elif (sign == None and value != None) or (sign == '-' and value == '0'):
-            factors.append(value)
+        elif (sign == '' and value != '') or (sign == '-' and value == '0'):
+            # Try casting to an int. If ValueError cast to a float.
+            try:
+                factors.append(int(value))
+            except ValueError:
+                factors.append(float(value))
+        # Else if there is a '-' sign and some value the factor is negative.
+        elif sign == '-' and value != '':
+            # Try casting to an int. If ValueError cast to a float.
+            try:
+                factors.append(int(sign + value))
+            except ValueError:
+                factors.append(float(sign + value))
         else:
-            factors.append(value)
+            # Try casting to an int. If ValueError cast to a float.
+            try:
+                factors.append(int(value))
+            except ValueError:
+                factors.append(float(value))
 
+    print('factors:', factors)
     return factors
 
 
-def extract_constraints():
+def extract_constraints(constraints):
+    """ Return eqin
+
+    Eqin is a list containing the type of the constraints e.g. <= .
+    """
+
+    eqin = []
+
+    # Iterate over the constraints.
+    for constraint in constraints:
+        # Constraint type is a match object containing the type of the constraint.
+        c_type = re.search(r'<=|>=|=', constraint)
+        if c_type.group() == '<=':
+            eqin.append(-1)
+        elif c_type.group() == '>=':
+            eqin.append(1)
+        else:
+            eqin.append(0)
+
+    return eqin
+
+
+def extract_bconstants(constraints):
+    """ Return b
+
+    Extracts the left hand side constants of a linear equation.
+    """
+
+    b = []
+
+    # Iterate over the constraints
+    for constraint in constraints:
+        match = re.search(r'<=\-?\d+|>=\-?\d+|=\-?\d+', constraint)
+        # If the type of the constraint is '>=' or '<=' slice
+        # the first 2 elements of the string to get the constant.
+        if match.group()[0] == '>' or match.group()[0] == '<':
+            b.append(int(match.group()[2:]))
+        # Else slice only the first element of the string to get the constant.
+        else:
+            b.append(int(match.group()[1:]))
+
+    return b
+
+
+def save_matrixes():
     pass
 
 
@@ -141,12 +199,26 @@ def main():
     c = []  # factors of the objective function
     Eqin = []  # constraints:-1 (<=), 1 (>=), 0 (=)
 
-    data = load_data()
+    data = load_linear_problem()
     check_format(data)
     minmax = get_lp_type(data[0])
 
     c = extract_factors(data[0][3:])
-    print(c)
+
+    for constraint in data[1:]:
+        if st_regex.match(constraint):
+            st_len = get_st_len(constraint)
+            A.append(extract_factors(constraint[st_len:]))
+        else:
+            A.append(extract_factors(constraint))
+
+    Eqin = extract_constraints(data[1:])
+    b = extract_bconstants(data[1:])
+
+    print('c = ', c)
+    print('A = ', A)
+    print('Eqin = ', Eqin)
+    print('b = ', b)
 
 
 if __name__ == '__main__':
